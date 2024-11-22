ae1ec07829cd270ee3d40898dc1425a5e2459ab4
Rohini R <rohinimangai14@gmail.com>
2024-11-22 15:29:31 +0530

    OOPS - comparison objects

--- src/ObjectOrientedProgramming/Generics/comparing/Main.java
+++ src/ObjectOrientedProgramming/Generics/comparing/Main.java
@@ -1,10 +1,11 @@
 package ObjectOrientedProgramming.Generics.comparing;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Student Rohini = new Student(9781,99.3f);
+        Student Rohini = new Student(9781,99.35f);
         Student Mangu = new Student(9780,99.2f);
         Student Raja = new Student(9837,95.3f);
         Student Arul = new Student(9782,79.3f);
@@ -13,7 +14,13 @@ public class Main {
 
         Student [] list = {Rohini,Mangu,Raja,Arul,Vishnu};
         System.out.println(Arrays.toString(list));
-        Arrays.sort(list);
+//        Arrays.sort(list, new Comparator<Student>(){
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return -(int)(o1.marks - o2.marks);
+//            }
+//        }); - instead of this we can use lambda expressions
+        Arrays.sort(list, (o1,o2) -> (int)(o1.marks - o2.marks));
         System.out.println(Arrays.toString(list));
 //        if(Rohini.marks < Mangu.marks) {
 //            System.out.println("Mangu has more marks");
